28ef0639bd0f0a28a676d8e239f2e50f2d35c492
SakshiTiwari07 <118429573+SakshiTiwari07@users.noreply.github.com>
2022-11-19 01:02:12 +0530

    update footer

--- src/Components/sections/footer.js
+++ src/Components/sections/footer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import styled from 'styled-components';
+import Button from "../Button";
+import Logo from "../Logo";
 
+import Facebook from "../../Icons/Facebook";
+import Instagram from "../../Icons/Instagram";
+import Twitter from "../../Icons/Twitter";
+import LinkedIn from "../../Icons/LinkedIn";
 
 const Section = styled.section`
 min-height: 100vh;
@@ -30,6 +36,55 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 `
+const IconList = styled.div`
+display: flex;
+align-items: center;
+margin: 1rem auto;
+
+&>*{
+    padding-right: 0%.5rem;
+    transition: all 0.2s ease;
+
+    &:hover{
+        transform: scale(1,2);
+    }
+}
+`
+const MenuItems = styled.ul`
+list-style: none;
+width: 50%;
+display: grid;
+grid-template-columns: repeat(2,1fr);
+grid-template-rows: repeat(3,1fr);
+grid-gap: 1rem;
+`
+const Item = styled.li`
+width: fit-content;
+cursor: pointer;
+
+&::after{
+    content: ' ';
+    display: block;
+    width: 0;
+    height: 2px;
+    background-color: ${props => props.theme.text};
+    transform: widht 0.3s ease;
+}
+&:hover ::after{
+    width: 100%;
+}
+`
+const Bottom = styled.div`
+width: 75%;
+margin: 0 auto;
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+a{
+    text-decoration:underline ;
+}
+`
 
 const Footer = () => {
     return(
@@ -38,8 +93,49 @@ const Footer = () => {
             <Container>
         <Left>
         <Logo/>
+        <IconList>
+            <a href="http://facebook,com" target="_blank"
+            rel="noopener noreferrer"
+            >
+                <Facebook />
+            </a>
+            <a href="http://instagram,com" target="_blank"
+            rel="noopener noreferrer"
+            >
+                <Instagram />
+            </a>
+            <a href="http://twitter,com" target="_blank"
+            rel="noopener noreferrer"
+            >
+                <Twitter />
+            </a>
+            <a href="http://linkedin,com" target="_blank"
+            rel="noopener noreferrer"
+            >
+                <LinkedIn />
+            </a>
+        </IconList>
         </Left>
+        <MenuItems>
+        <Items>Home</Items>
+        <Items>About</Items>
+        <Items>Roadmap</Items>
+
+        <Items>Showcase</Items>
+        <Items>About</Items>
+        <Items>Faq</Items>
+        </MenuItems>
             </Container>
+            <Button>
+                <span>
+                    &copy; {new Date().getFullYear()} weirdos Club. All rights reserved.
+                </span>
+                <span>
+                    Made with &#10084; by <a href="http;//youtube.com/codebucks" target="_blank" rel="noopener noreferrer">
+                        CodeBucks
+                    </a>
+                </span>
+            </Button>
         </Section>
     )
 }
